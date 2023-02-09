af7b2e17b0a4fa78e26c596a8305c6452c32d1c4
282330332 <282330332@qq.com>
2023-02-09 20:20:34 +0800

    添加测试和非测试逻辑

--- app/src/main/java/com/cold/flavorsdemo/MainActivity.java
+++ app/src/main/java/com/cold/flavorsdemo/MainActivity.java
@@ -3,13 +3,24 @@ package com.cold.flavorsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.lib_common.DebugHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTest1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
+        tvTest1 = findViewById(R.id.tv_test1);
+    }
+
+    public void onDebug(View v) {
+        String test = DebugHandler.getInstance().getText();
+        tvTest1.setText(test);
     }
 }
---
+++ lib_common/src/main/java/com/example/lib_common/IDebugHandler.java
@@ -0,0 +1,8 @@
+package com.example.lib_common;
+
+public interface IDebugHandler {
+
+    boolean isDebug();
+    String getText();
+
+}
